e4170aff1785a7c09c81d3427506b2e80d54278f
kga-lab <kga.lab.iisc@gmail.com>
2026-06-30 10:33:47 +0530

    Add files via upload

---
+++ Nesterenkonia/AA-module/simu-water/PILA/myscript_preparesystem.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+EXE=gmx
+molecule='PILA'
+
+############
+
+$EXE editconf -f ${molecule}.gro -box 8 8 8 -center 4.0 4.0 4.0 -o ${molecule}.gro
+$EXE grompp -f step_mini.mdp -c ${molecule}.gro -p topol.top -o system_mini.tpr -maxwarn 2
+
+$EXE mdrun -v -deffnm system_mini
+
+$EXE solvate -cp system_mini.gro -cs spc216.gro -p topol.top -o system_sol.gro -maxsol 16903
+
+$EXE  grompp -f step_mini.mdp -c system_sol.gro -p topol.top -o system_mini.tpr -r system_sol.gro -maxwarn 1
+
+$EXE genion -s system_mini.tpr -p topol.top -pname K -pq 1 -nname CL -nq -1 -neutral -o system.gro
+
+$EXE make_ndx -f system.gro -o index.ndx  
