d1b27b906118a22e8de364dd5a0edde86ce33a18
kga-lab <kga.lab.iisc@gmail.com>
2026-06-30 13:36:13 +0530

    Add files via upload

---
+++ Nesterenkonia/CG-module/simu-water/PGLA/analysis/myscript_compute_sasa.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+gmx sasa -s ../system_prod.tpr -f ../system_prod.xtc -n ../index.ndx -o sasa-PGLA_CG.xvg -probe 0.191 -ndots 4800 -b 1000000 << EOF
+PGLA
+EOF
---
+++ Nesterenkonia/CG-module/simu-water/PGLA/myscript_solvate.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+EXE=gmx_mpi
+###########SOLVATE
+copy PGLA_cg.gro from AA-module/simu-water/PGLA/analysis/
+
+$EXE editconf -f PGLA_cg.gro -box 10 10 10 -center 5 5 5 -o PGLA_cg_fix.gro 
+
+$EXE solvate -cp PGLA_cg_fix.gro -cs water.gro -p topol.top -o system.gro -maxsol 8715  2>&1 |tee  out1.txt   
+#Note we are keeping same number of water molecules as in other lipid systems have
+
+#wn=$(awk '/Generated solvent/ {print $4}' out1.txt)
+#echo "W    $wn" >> topol.top
+
+##########ADD ions
+$EXE grompp -f step_mini.mdp -c system.gro -p topol.top -o system_mini.tpr -r system.gro -maxwarn 1
+
+echo "W" | $EXE genion -s system_mini.tpr -p topol.top -o system_fix.gro -pname K -pq 1 -nname CL -nq -1 -neutral  
+
+$EXE make_ndx -f system_fix.gro -o index.ndx  
