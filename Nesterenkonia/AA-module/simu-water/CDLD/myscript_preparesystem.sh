6e1146512aa82a238a2c49f4ccec0e68cece4427
kga-lab <kga.lab.iisc@gmail.com>
2026-06-30 09:52:29 +0530

    Add files via upload

---
+++ Nesterenkonia/AA-module/simu-water/CDLD/myscript_preparesystem.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+EXE=gmx
+molecule='CDLD'
+
+sed -i "s/CDLC/$molecule/g" step_eq1.mdp
+sed -i "s/CDLC/$molecule/g" step_eq2.mdp
+sed -i "s/CDLC/$molecule/g" step_eq3.mdp
+sed -i "s/CDLC/$molecule/g" step_eq4.mdp
+sed -i "s/CDLC/$molecule/g" step_prod.mdp
+
+
+############
+
+$EXE editconf -f ${molecule}.gro -box 8 8 8 -center 4.0 4.0 4.0 -o ${molecule}.gro
+$EXE grompp -f step_mini.mdp -c ${molecule}.gro -p topol.top -o system_mini.tpr -maxwarn 2
+
+$EXE mdrun -v -deffnm system_mini
+
+$EXE solvate -cp system_mini.gro -cs spc216.gro -p topol.top -o system_sol.gro -maxsol 16904 -scale 0.56
+
+$EXE  grompp -f step_mini.mdp -c system_sol.gro -p topol.top -o system_mini.tpr -r system_sol.gro -maxwarn 1
+
+$EXE genion -s system_mini.tpr -p topol.top -pname K -pq 1 -nname CL -nq -1 -neutral -o system.gro
+
+$EXE make_ndx -f system.gro -o index.ndx  
