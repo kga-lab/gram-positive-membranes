7e47aa984feccd9b703a355a8f5d5aa6ff021aa9
kga-lab <kga.lab.iisc@gmail.com>
2026-07-08 17:50:14 +0530

    Add files via upload

---
+++ Nesterenkonia/CG-module/simu-memb-large/mypython-bendingmodulus.py
@@ -0,0 +1,59 @@
+import pandas as pd
+import numpy as np
+import math
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1 import make_axes_locatable
+from matplotlib.ticker import FormatStrFormatter
+from scipy.optimize import curve_fit
+from sklearn.metrics import r2_score
+
+plt.rcParams['axes.linewidth']=2.5
+plt.rcParams['figure.dpi'] = 300
+plt.rcParams['savefig.dpi'] = 300
+
+def func(x,D,c):
+    y = 1.0/((D*(x**4)) + (c*(x**2)))
+    return y
+
+################
+data = np.loadtxt("Skavg.dat",comments=['@','#'])
+
+x=data[:,0]
+y=data[:,1]
+x1=data[:,0][data[:,0]<0.45] #for fits where q < 0.45
+y1=data[:,1][data[:,0]<0.45]
+p, co = curve_fit(func,x1,y1)
+perr = np.sqrt(np.diag(co))
+
+D=p[0]
+c=p[1]
+D_stderr=perr[0]
+c_stderr=perr[1]
+print(D,D_stderr,c,c_stderr) 
+yf= func(x1,D,c)
+R2fit=r2_score(y1,yf)
+print(R2fit)
+
+####################PLOT 
+fig=plt.figure() #figsize=(30,20))  #(48/28)
+row=1
+col=1
+#grid=plt.GridSpec(row,col,wspace=0.18,hspace=0.30,width_ratios=[2,2,2], height_ratios=[2,2,2])  #wspace=0.15
+grid=plt.GridSpec(row,col,wspace=0.18,hspace=0.30)
+ax=plt.subplot(grid[0])
+plt.scatter(data[:,0],data[:,1],color='blue',marker='o',s=50)
+#plt.scatter(data2[:,0],data2[:,1],color='crimson',marker='o',s=30)
+
+plt.plot(x1,yf,color='crimson',lw=2,ls='dotted')
+plt.tick_params(direction='in',labelsize=15,length=4,width=1,pad=5)
+plt.tick_params(which='minor',direction='in',labelsize=15,length=4,width=1,pad=5)
+plt.xscale('log')
+plt.yscale('log')
+ax.set_xlim([0.1,5])
+ax.set_title(r'$\kappa$ = '+str(np.round(D,2))+' kT',fontsize=20,x=0.5,y=0.8)
+#plt.legend(prop={'size':40},frameon=False,handlelength=2,loc=1,ncol=4)
+plt.xlabel(r'q (1/nm)',labelpad=10,fontsize=20)
+plt.ylabel(r'S(q)',fontsize=20,labelpad=10)
+plt.tight_layout
+plt.savefig('Fig-bendingmodulus-nesterenkonia.png',dpi=300,format='png',bbox_inches = 'tight', pad_inches = 0.1,facecolor='w')
+plt.show()
---
+++ Nesterenkonia/CG-module/simu-memb-large/structurefactor2D.f
@@ -0,0 +1,94 @@
+!=======================================================================!
+!This program computes the structure factor S(q) data from heights of 
+!membrane lipids. The CG beads P1 in CARDIOLIPIN and PO4 in PG/PI lipids
+!are chosen as headgroups.
+!=======================================================================!
+              PROGRAM Structurefactor2D
+                implicit none
+                integer N,maxite,c1,c2,c,i,ite,Nt,j
+                parameter (N =4096,maxite=20000) ! heads 
+                double precision xp(N),yp(N),zp(N),xc(N),yc(N),zc(N)
+                double precision Lx,Ly,Lz
+                double precision xu(N),yu(N),zu(N),xl(N),yl(N),zl(N) 
+                COMPLEX sumusk,sumlsk,sumsk
+                integer k1,k2,k1max,k2max,kk2,nmodes 
+                parameter (k1max = 16,k2max = 16,nmodes=405) 
+                double precision kx,ky,theta,facx,facy,pi,Twopi
+                double precision Sk(nmodes),Skavg(nmodes),q(nmodes)
+                parameter (pi =  3.141592653589793d0)
+                character*6 dummy,path
+                character*8 resnam
+                character*7 atom
+                path = '../'
+!=======================================================================!
+!This program computes S(k) = < h(k) h(-k)> *A
+!=======================================================================!
+               Twopi = 2.d0*pi
+            open(11,file='traj_heads.gro') !trajectory of lipid headgroups.
+            open(12,file='traj_com.gro') !trajectory of com of individual lipid.
+
+            open(21,file='Skavg-v4.dat')
+
+!----------------------------------------------------------!
+               Skavg(:) = 0.d0
+                q(:) = 0.d0
+             do ite=1,maxite
+               read(11,*) dummy
+               read(11,*) Nt
+                do i=1,Nt
+             read(11,'(a8,a7,i5,3f8.3)') resnam,atom,j,xp(i),yp(i),zp(i)
+                enddo
+                read(11,*) Lx,Ly,Lz
+
+               read(12,*) dummy
+               read(12,*) Nt
+                do i=1,Nt
+             read(12,'(a8,a7,i5,3f8.3)') resnam,atom,j,xc(i),yc(i),zc(i)
+                enddo
+               read(12,*) Lx,Ly,Lz
+               facx = Twopi/Lx
+               facy = Twopi/Ly
+            !--------Separate lipids into upper and lower leaflets
+        CALL upperlower(N,Nt,xp,yp,zp,xc,yc,zc,xu,yu,zu,xl,yl,zl,c1,c2) 
+               write(*,*) c1,c2
+            !-----x-y Fourier Transform ------!
+                   c = 0;
+                do k2 = -k2max,k2max
+                  ky = facy*dble(k2)
+                 do k1 = -k1max,k1max
+                   kx = facx*dble(k1)
+                   kk2 = (k1**2) + (k2**2)
+                  if(kk2 .le. 128) then !this corresponds qmax 
+                    c = c + 1
+                    q(c) = dsqrt((kx**2) + (ky**2))
+                   !----FT of zu----: sum zu*exp(i*2*pi*k/L * r)
+                   !----FT of zl----: sum zl*exp(i*2*pi*k/L *r)
+                  sumusk = cmplx(0.d0,0.d0)
+                  sumlsk = cmplx(0.d0,0.d0)
+                  do i=1,c1  !c1 no of lipids are in upperleaflet
+                   theta = (kx*xu(i)) + (ky*yu(i))
+                sumusk = sumusk + (zu(i)*cmplx(dcos(theta),dsin(theta)))
+                  enddo
+                  do i=1,c2  !c1 no of lipids are in lowerleaflet
+                   theta = (kx*xl(i)) + (ky*yl(i))
+                sumlsk = sumlsk + (zl(i)*cmplx(dcos(theta),dsin(theta)))
+                  enddo
+                   sumsk = 0.5d0*((sumusk/dble(c1))+(sumlsk/dble(c2))) 
+                   Sk(c) = sumsk*conjg(sumsk)
+                   !----- Time average -----!
+                   Skavg(c) = Skavg(c) + Sk(c)
+                  endif
+               enddo !k1
+              enddo !k2
+                     write(*,*) 'c modes:', ite, c
+             enddo !ite    
+                   Skavg(:) =  Skavg(:)* (Lx*Ly/dble(maxite))
+                 do i=1,nmodes
+                    write(21,*) q(i), Skavg(i)
+                  enddo
+                  close(11)
+                  close(12)
+                  close(21)
+               END PROGRAM
+!=======================================================================!
+               include 'upperlower.f'
